f604d610cfecfe1d9c9847e314a47e5313e4d4ba
OrangeXC <842012622@qq.com>
2017-06-22 14:32:39 +0800

    Fixed comment only show two line overflow hidden

--- components/Comment.vue
+++ components/Comment.vue
@@ -2,8 +2,8 @@
   <mu-list>
     <mu-sub-header>全部评论</mu-sub-header>
     <template v-for="item in comments">
-      <mu-list-item :title="item.member.username">
-        <mu-avatar :src="item.member.avatar_normal" slot="leftAvatar"/>
+      <mu-list-item :title="item.member.username" :describeLine="1000">
+        <mu-avatar :src="item.member.avatar_normal" @click="toMember(item.member.username)" slot="leftAvatar"/>
         <div v-html="item.content_rendered" slot="describe"></div>
       </mu-list-item>
       <mu-divider inset />
@@ -19,6 +19,11 @@ export default {
     comments: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    toMember (name) {
+      this.$router.push(`/member/${name}`)
     }
   }
 }
