1709f696056b99f49db74eb480850b947db085df
OrangeXC <842012622@qq.com>
2018-09-13 15:25:23 +0800

    Remove file user.js

--- users.js
+++
@@ -1,8 +0,0 @@
-var list = document.querySelectorAll('.remark_name')
-var user = []
-
-for (var i = 0; i < list.length; i++) {
-  user.push(`${list[i].getAttribute('data-fakeid')} ${list[i].text}`)
-}
-
-console.log(user.join('\n'))
